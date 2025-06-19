ed29dcab1f2f4faa526ca554dbc3fe92363cbd8e
C1921 <w915504817@gmail.com>
2025-06-19 16:33:59 +0800

    重构：将主题控制器抽取为组件

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { onMounted } from 'vue';
 import CardContainer from './components/CardContainer.vue';
 import ProgressStep from './components/ProgressStep.vue';
+import ThemeController from './components/ThemeController.vue';
 
 onMounted(() => {
   // 初始化其他组件
@@ -11,11 +12,7 @@ onMounted(() => {
 
 <template>
   <div class="p-2 bg-base-200">
-    <label class="swap swap-rotate">
-      <input type="checkbox" value="light" class="theme-controller" />
-      <span class="swap-off icon-[tabler--sun] size-7"></span>
-      <span class="swap-on icon-[tabler--moon] size-7"></span>
-    </label>
+    <ThemeController />
     <ProgressStep :current-step="10" :total-steps="20" :is-completed="false" />
     <CardContainer />
   </div>
---
+++ src/components/ThemeController.vue
@@ -0,0 +1,11 @@
+<template>
+  <label class="swap swap-rotate">
+    <input type="checkbox" value="light" class="theme-controller" />
+    <span class="swap-off icon-[tabler--sun] size-7"></span>
+    <span class="swap-on icon-[tabler--moon] size-7"></span>
+  </label>
+</template>
+
+<script setup lang="ts">
+// 可以添加更多功能，如记住用户主题偏好等
+</script> 
