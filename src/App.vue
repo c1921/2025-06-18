df2aa3567b3f99adca93b5887c548399b66437f5
C1921 <w915504817@gmail.com>
2025-06-19 17:37:04 +0800

    新增：创建StatusBar组件

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { onMounted } from 'vue';
 import CardContainer from './components/CardContainer.vue';
 import ProgressStep from './components/ProgressStep.vue';
 import ThemeController from './components/ThemeController.vue';
+import StatusBar from './components/StatusBar.vue';
 
 onMounted(() => {
   // 初始化其他组件
@@ -12,8 +13,11 @@ onMounted(() => {
 
 <template>
   <div class="p-2 bg-base-200">
+
     <ThemeController />
     <ProgressStep :current-step="10" :total-steps="20" :is-completed="false" />
+    <StatusBar />
     <CardContainer />
+    
   </div>
 </template>
---
+++ src/components/StatusBar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="bg-base-100 px-6 py-2 grid grid-cols-2 gap-8 rounded-box">
+        <div class="flex flex-col gap-3">
+            <div class="flex flex-col gap-1">
+                <span class="progress-label ms-[calc(0%-1.25rem)]">0%</span>
+                <div class="progress h-2" role="progressbar" aria-label="25% Progressbar" aria-valuenow="25"
+                    aria-valuemin="0" aria-valuemax="100">
+                    <div class="progress-bar progress-primary w-0/100"></div>
+                </div>
+            </div>
+            <div class="progress w-3/5" role="progressbar" aria-label="25% Progressbar" aria-valuenow="25"
+                aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar progress-warning w-1/4"></div>
+            </div>
+
+        </div>
+
+        <div class="flex flex-col gap-3 rotate-y-180">
+            <div class="flex flex-col gap-1">
+                <span class="progress-label ms-[calc(100%-1.25rem)] rotate-y-180">100%</span>
+                <div class="progress h-2" role="progressbar" aria-label="25% Progressbar" aria-valuenow="25"
+                    aria-valuemin="0" aria-valuemax="100">
+                    <div class="progress-bar progress-primary w-100/100"></div>
+                </div>
+            </div>
+            <div class="progress w-3/5" role="progressbar" aria-label="25% Progressbar" aria-valuenow="25"
+                aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar progress-warning w-1/4"></div>
+            </div>
+
+        </div>
+    </div>
+</template>
