f1a533463fc0a77d493eaade04230cde578b4927
C1921 <w915504817@gmail.com>
2025-06-19 14:23:16 +0800

    新增：创建ProgressStep.vue组件

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import CardContainer from './components/CardContainer.vue';
 import { onMounted } from 'vue';
+import CardContainer from './components/CardContainer.vue';
+import ProgressStep from './components/ProgressStep.vue';
 
 onMounted(() => {
   // 初始化其他组件
@@ -11,5 +12,6 @@ onMounted(() => {
 <template>
   <div class="p-2 bg-base-200">
     <CardContainer />
+    <ProgressStep :current-step="10" :total-steps="20" :is-completed="false" />
   </div>
 </template>
---
+++ src/components/ProgressStep.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="flex items-center gap-x-1">
+    <div 
+      v-for="step in totalSteps" 
+      :key="step"
+      class="progress-step" 
+      :class="step <= currentStep ? 'bg-primary' : 'bg-primary/10'"
+      role="progressbar" 
+      aria-label="Progressbar" 
+      :aria-valuenow="step <= currentStep ? 100 : 0"
+      aria-valuemin="0" 
+      aria-valuemax="100"
+    ></div>
+    <p class="size-6">
+      <span 
+        :class="[
+          'icon-[tabler--circle-check-filled] size-6', 
+          isCompleted ? 'text-primary' : 'text-primary/10'
+        ]"
+      ></span>
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+interface Props {
+  currentStep: number;
+  totalSteps: number;
+  isCompleted?: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  currentStep: 0,
+  totalSteps: 10,
+  isCompleted: false
+});
+</script>
